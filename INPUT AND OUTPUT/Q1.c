d4aafb09257e2ea15bebb33972e500e902c7d40e
thirukumara1221-netizen <thirukumara1221@gmail.com>
2025-08-30 10:21:14 +0530

    Add files via upload

---
+++ INPUT AND OUTPUT/Q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+
+{
+	int numerator=5;
+	int dinamator=7;
+	printf("fractional is :%d/%d\n",numerator ,dinamator);
+	float result= (float) numerator/dinamator;
+	printf("decimal value:%.4f\n",result);
+	return 0;
+
+	
+}
---
+++ INPUT AND OUTPUT/Q2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	int n=5;
+	int d=56;
+	printf("fritional is",%d\n\)
+}
